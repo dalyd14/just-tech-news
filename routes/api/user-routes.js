0be30e0f52c625ad13752af51339d7015a5a00f1
David Daly <dalyd14@gmail.com>
2021-01-27 12:47:42 -0500

    built the vote model; had many-to-many relationship

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-const { User } = require('../../models')
+const { User, Post, Vote } = require('../../models')
 
 // GET ALL
 router.get('/', (req, res) => {
@@ -20,7 +20,19 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if(!dbUserData) {
