5f86e0f5f5818c0d9ed2ee487e410b2bc5e021ef
David Daly <dalyd14@gmail.com>
2021-01-29 09:54:48 -0500

    finished added the comment model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection')
-const { Post, User, Vote } = require('../../models')
+const { Post, User, Vote, Comment } = require('../../models')
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -16,6 +16,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -44,6 +52,14 @@ router.get('/:id', (req, res) => {
             ]
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
