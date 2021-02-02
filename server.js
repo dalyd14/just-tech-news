b7f3557712671511556d6cb931686b75e227430f
David Daly <dalyd14@gmail.com>
2021-02-02 09:35:44 -0500

    added login route; fixed css static file serving; adding frontend js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const routes = require('./controllers');
 const path = require('path');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-
-app.use(routes);
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const routes = require('./controllers');
+app.use(routes);
+
+const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
