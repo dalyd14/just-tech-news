8c4a849b050b341711a1bb06b4667f16d8e415b0
David Daly <dalyd14@gmail.com>
2021-02-02 08:35:19 -0500

    added files, template, resturctured folders

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const path = require('path');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +12,11 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening...'))
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
 })
