f77759b79530ec86615c3209990dcf6fa1b4cee9
David Daly <dalyd14@gmail.com>
2021-02-04 17:33:39 -0500

    added login and logout functionality

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+
 const routes = require('./controllers');
 app.use(routes);
 
